e36d559477150acd8464c5a4fac3acfad53cb24f
Kobe Ryan <koberyan@comcast.net>
2019-07-14 20:41:29 -0700

    added Game Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,21 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2){
-  if (cardsInPlay[1] === cardsInPlay[0]) {
-    alert("you found a match!");
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    console.log("You found a match!");
   } else {
-    alert("Sorry try again");
+    console.log("Sorry, try again.");
   }
-
 }
 
-console.log("User flipped " + cardsInPlay);
+
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length === 2){
+    checkForMatch()
+    }
+  }
+  flipCard([0])
+  flipCard([2])
