755304441378c9bf6ed939af1f32eb026d43e34e
Kobe Ryan <koberyan@comcast.net>
2019-07-14 22:26:27 -0700

    game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]){
@@ -11,7 +32,9 @@ var checkForMatch = function () {
 
 
 var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId]);
   if (cardsInPlay.length === 2){
     checkForMatch()
